5ae2f7bdaeef086405df5f7ee517c85147c03d08
Lorena Medeiros <lomedeiros@proton.me>
2024-08-28 17:17:14 -0300

    feat: configure general security

---
+++ src/main/java/io/github/lsmcodes/notes_api/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,78 @@
+package io.github.lsmcodes.notes_api.configuration.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.github.lsmcodes.notes_api.filter.AuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class SecurityConfiguration {
+
+    @Autowired
+    private AuthenticationFilter authenticationFilter;
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    private static final String[] WHITELIST = {
+            "/h2-console/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
+    @Bean
+    @Profile({"dev", "test"})
+    SecurityFilterChain securityFilterChainDevTest(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(WHITELIST).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/notes-api/authentication").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/notes-api/users").permitAll()
+                        .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
+    SecurityFilterChain securityFilterChainProd(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/notes-api/authentication").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/notes-api/users").permitAll()
+                        .requestMatchers("/notes-api/users").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/notes-api/notes", "/notes-api/notes/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
